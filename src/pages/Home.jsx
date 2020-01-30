cfb1733439177e1a7b479c0f0e436e83ab6f1fac
Aron <aron.braggans@gmail.com>
2020-01-29 20:06:42 -0600

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { DateTime } from 'luxon';
-import { Section } from './../organisms/section';
+import React, { useState, useEffect } from "react";
+import { DateTime } from "luxon";
+import { Section } from "./../organisms/section";
 
-import trail1 from './../img/tmp_trail_1.jpg';
-import trail2 from './../img/tmp_trail_2.jpg';
-import trail3 from './../img/tmp_trail_3.jpg';
+import trail1 from "./../img/tmp_trail_1.jpg";
+import trail2 from "./../img/tmp_trail_2.jpg";
+import trail3 from "./../img/tmp_trail_3.jpg";
 
 function displayStatus(status) {
-  return status === 'Wet' || status === 'Melting'
+  return status === "Wet" || status === "Melting"
     ? `Closed - ${status}`
     : status;
 }
@@ -16,7 +16,7 @@ export function Home() {
   const [trails, setTrails] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.morcmtb.org/v1/trails')
+    fetch("https://api.morcmtb.info/v1/trails")
       .then(res => res.json())
       .then(json => {
         setTrails(json);
@@ -28,9 +28,9 @@ export function Home() {
     <Section>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
         }}
       >
         {trails.map((t, i) => {
@@ -53,47 +53,47 @@ function Trail(props) {
   return (
     <div
       style={{
-        maxWidth: '720px',
-        border: 'solid 1px gray',
-        marginBottom: '1rem',
-        boxShadow: '0 0 1rem 0 rgba(0, 0, 0, 0.11)'
+        maxWidth: "720px",
+        border: "solid 1px gray",
+        marginBottom: "1rem",
+        boxShadow: "0 0 1rem 0 rgba(0, 0, 0, 0.11)"
       }}
     >
       <img src={image} className="trailImage" alt="" />
-      <div style={{ padding: '0rem .5rem' }}>
+      <div style={{ padding: "0rem .5rem" }}>
         <h1 style={{}} className="title">
           {trail.trailName}
         </h1>
       </div>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexDirection: 'row',
-          alignItems: 'center',
-          padding: '0rem .5rem'
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          alignItems: "center",
+          padding: "0rem .5rem"
         }}
       >
         <h2
           style={{
-            fontWeight: 'bold'
+            fontWeight: "bold"
           }}
         >
           {displayStatus(trail.trailStatus)}
         </h2>
         <div
           style={{
-            fontSize: 'smaller'
+            fontSize: "smaller"
           }}
         >
-          Updated:{' '}
+          Updated:{" "}
           <time>
             {DateTime.fromMillis(trail.updatedAt).toLocaleString({
-              weekday: 'short',
-              month: 'short',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit'
+              weekday: "short",
+              month: "short",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit"
             })}
           </time>
         </div>
@@ -101,19 +101,19 @@ function Trail(props) {
 
       <div
         style={{
-          padding: '.5rem',
-          display: 'flex',
-          flexDirection: 'column'
+          padding: ".5rem",
+          display: "flex",
+          flexDirection: "column"
         }}
       >
         {trail.description}
       </div>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          margin: '.5rem'
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          margin: ".5rem"
         }}
       >
         <address>
@@ -127,7 +127,3 @@ function Trail(props) {
     </div>
   );
 }
-
-// "trailId": "3b716fa1-af6b-4aa8-aff1-11ff473072d1",
-// "createdAt": 1519053269271,
-// "updatedBy": "us-east-1:399937b7-68ae-49a0-bb4e-ababb1888974",
